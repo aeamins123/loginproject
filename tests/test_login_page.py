b08829580d6df75edec453751a5a511e4d42a9ac
Amina <aeamins@gmail.com>
2019-05-17 22:16:12 +0530

    before and after script added in .travis.yml

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -7,7 +7,7 @@ class LoginPageTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+        cls.driver = webdriver.Chrome(executable_path="drivers/chromedriver.exe")
         cls.driver.implicitly_wait(10)
         cls.driver.maximize_window()
 
