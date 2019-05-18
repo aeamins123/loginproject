eb1dd62efa34c5e1069d12f200c5ec9a6934b034
Amina <aeamins@gmail.com>
2019-05-19 00:47:04 +0530

    Deployment to heroku

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,5 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install @heroku-cli/plugin-container-registry
 heroku container:login
-heroku container:push web –a $HEROKU_APP_NAME
+echo $HEROKU_APP_NAME | heroku container:push web –-app-stdin
 heroku container:release web –a $HEROKU_APP_NAME
