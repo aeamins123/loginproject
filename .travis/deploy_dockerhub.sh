ec62863fa6e2a85123a023fc4f138b5ec94f0a34
Amina <aeamins@gmail.com>
2019-05-18 12:38:33 +0530

    script added for running the application in localhost

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,5 +1,5 @@
 echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 TAG=”latest”
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+docker build -f Dockerfile -t "loginproject":"latest"
 docker tag $TRAVIS_REPO_SLUG $DOCKER_REPO
 docker push $DOCKER_REPO
