6dce4de94af426e288437cc33daa4b3047172534
Amina <aeamins@gmail.com>
2019-05-18 13:58:39 +0530

    script added for running the application in localhost

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -2,5 +2,5 @@ echo "$DOCKER_PASS" | docker login -u "$DOCKER_USER" --password-stdin
 TAG=latest
 NAME=loginproject
 docker build -f Dockerfile -t $NAME:$TAG .
-docker tag $NAME $DOCKER_REPO
-docker push $DOCKER_REPO
+docker tag $NAME  aeamins/aeamins:myfirstpush
+docker push aeamins/aeamins:myfirstpush
