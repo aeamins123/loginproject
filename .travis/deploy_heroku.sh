08f7c4982f308dd682f878022b7aa0e3fdcd81bc
Amina <aeamins@gmail.com>
2019-05-20 01:44:47 +0530

    Changes in app.py for run()

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -4,5 +4,4 @@ heroku container:login
 docker build -t registry.heroku.com/${HEROKU_APP_NAME}/web .
 docker push registry.heroku.com/${HEROKU_APP_NAME}/web
 heroku container:release web
-heroku open --app ${HEROKU_APP_NAME}
 
